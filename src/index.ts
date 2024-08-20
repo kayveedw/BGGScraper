f33bbe9cb007fdf3c576ad6aed4ea307e7e8c486
KayVeeDW <github@kayvee.org>
2024-08-20 18:08:39 +0100

    Accept Start index as a cmd line argument

--- src/index.ts
+++ src/index.ts
@@ -43,151 +43,140 @@ class BBGGame {
 async function main() {
 	const baseURL: string = 'https://api.geekdo.com/xmlapi/boardgame/'; // Use XML API see https://boardgamegeek.com/wiki/page/BGG_XML_API for details.
 
-	const START_INDEX: number = 1000;
+	if (process.argv.length >= 2) {
+		const START_INDEX: number = Number(process.argv[2]);
+		// const START_INDEX: number = 1000;
 
-	const data: BBGGame[] = [];
+		const data: BBGGame[] = [];
 
-	for (let index: number = START_INDEX; index < START_INDEX + 1000; index++) {
-		// for (let index: number = 1; index <= 58000; index++) {
-		const response = await fetch(baseURL + '/' + index.toString());
+		for (
+			let index: number = START_INDEX;
+			index < START_INDEX + 1000;
+			index++
+		) {
+			const response = await fetch(baseURL + '/' + index.toString());
 
-		// const headers = await response.headers.raw();
-		const XMLData: string = await response.text();
+			// const headers = await response.headers.raw();
+			const XMLData: string = await response.text();
 
-		// Configure XMLParser
-		const alwaysArray = [
-			'boardgames.boardgame.name',
-			'boardgamedesigner',
-			'boardgameartist',
-			'boardgamepublisher',
-			'boardgamemechanic',
-			'boardgamecategory',
-			'boardgamefamily',
-		];
-		const options = {
-			ignoreAttributes: false,
-			attributeNamePrefix: '',
-			textNodeName: 'value',
-			isArray: (
-				name: string,
-				jpath: string,
-				isLeafNode: boolean,
-				isAttribute: boolean
-			) => {
-				if (alwaysArray.indexOf(jpath) !== -1) return true;
-				if (alwaysArray.indexOf(name) !== -1) return true;
-			},
-		};
+			// Configure XMLParser
+			const alwaysArray = [
+				'boardgames.boardgame.name',
+				'boardgamedesigner',
+				'boardgameartist',
+				'boardgamepublisher',
+				'boardgamemechanic',
+				'boardgamecategory',
+				'boardgamefamily',
+			];
+			const options = {
+				ignoreAttributes: false,
+				attributeNamePrefix: '',
+				textNodeName: 'value',
+				isArray: (
+					name: string,
+					jpath: string,
+					isLeafNode: boolean,
+					isAttribute: boolean
+				) => {
+					if (alwaysArray.indexOf(jpath) !== -1) return true;
+					if (alwaysArray.indexOf(name) !== -1) return true;
+				},
+			};
 
-		const parser = new XMLParser(options);
-		const root = parser.parse(XMLData);
-		// writeFile('./temp/' + index + '.json', JSON.stringify(root), (err) => {
-		// 	if (err) {
-		// 		console.log('err = ' + err);
-		// 	}
-		// });
+			const parser = new XMLParser(options);
+			const root = parser.parse(XMLData);
 
-		const currentGame: BBGGame = new BBGGame(index);
+			const currentGame: BBGGame = new BBGGame(index);
 
-		const boardGame = root.boardgames.boardgame;
+			const boardGame = root.boardgames.boardgame;
 
-		if (!boardGame['error']) {
-			if (boardGame['name']) {
-				for (let name of boardGame['name']) {
-					// boardGame.name.forEach((item: any) => {
-					if (name['primary']) {
-						currentGame.name = name['value'];
+			if (!boardGame['error']) {
+				if (boardGame['name']) {
+					for (let name of boardGame['name']) {
+						// boardGame.name.forEach((item: any) => {
+						if (name['primary']) {
+							currentGame.name = name['value'];
+						}
 					}
 				}
-			}
 
-			currentGame.description = boardGame['description'];
-			currentGame.image.href = boardGame['image'];
+				currentGame.description = boardGame['description'];
+				currentGame.image.href = boardGame['image'];
 
-			if (boardGame['boardgamedesigner']) {
-				for (let designer of boardGame['boardgamedesigner']) {
-					currentGame.designers.push({
-						id: designer.objectid,
-						name: designer.value,
-					});
+				if (boardGame['boardgamedesigner']) {
+					for (let designer of boardGame['boardgamedesigner']) {
+						currentGame.designers.push({
+							id: designer.objectid,
+							name: designer.value,
+						});
+					}
 				}
-			}
 
-			if (boardGame['boardgameartist']) {
-				for (let artist of boardGame['boardgameartist']) {
-					currentGame.artists.push({
-						id: artist.objectid,
-						name: artist.value,
-					});
+				if (boardGame['boardgameartist']) {
+					for (let artist of boardGame['boardgameartist']) {
+						currentGame.artists.push({
+							id: artist.objectid,
+							name: artist.value,
+						});
+					}
 				}
-			}
 
-			if (boardGame['boardgamepublisher']) {
-				for (let publisher of boardGame['boardgamepublisher']) {
-					currentGame.publishers.push({
-						id: publisher.objectid,
-						name: publisher.value,
-					});
+				if (boardGame['boardgamepublisher']) {
+					for (let publisher of boardGame['boardgamepublisher']) {
+						currentGame.publishers.push({
+							id: publisher.objectid,
+							name: publisher.value,
+						});
+					}
 				}
-			}
 
-			currentGame.publishedYear = boardGame['yearpublished'];
+				currentGame.publishedYear = boardGame['yearpublished'];
 
-			if (boardGame['boardgamesubdomain.value']) {
-				currentGame.type = boardGame['boardgamesubdomain.value'];
-			}
-
-			if (boardGame['boardgamecategory']) {
-				for (let category of boardGame['boardgamecategory']) {
-					currentGame.categories.push({
-						id: category.objectid,
-						name: category.value,
-					});
+				if (boardGame['boardgamesubdomain.value']) {
+					currentGame.type = boardGame['boardgamesubdomain.value'];
 				}
-			}
 
-			if (boardGame['boardgamemechanic']) {
-				for (let mechanic of boardGame['boardgamemechanic']) {
-					currentGame.mechanics.push({
-						id: mechanic.objectid,
-						name: mechanic.value,
-					});
+				if (boardGame['boardgamecategory']) {
+					for (let category of boardGame['boardgamecategory']) {
+						currentGame.categories.push({
+							id: category.objectid,
+							name: category.value,
+						});
+					}
 				}
-			}
 
-			if (boardGame['boardgamefamily']) {
-				for (let family of boardGame['boardgamefamily']) {
-					currentGame.families.push({
-						id: family.objectid,
-						name: family.value,
-					});
+				if (boardGame['boardgamemechanic']) {
+					for (let mechanic of boardGame['boardgamemechanic']) {
+						currentGame.mechanics.push({
+							id: mechanic.objectid,
+							name: mechanic.value,
+						});
+					}
 				}
+
+				if (boardGame['boardgamefamily']) {
+					for (let family of boardGame['boardgamefamily']) {
+						currentGame.families.push({
+							id: family.objectid,
+							name: family.value,
+						});
+					}
+				}
+
+				data.push(currentGame);
 			}
-
-			// console.log(JSON.stringify(currentGame));
-
-			// writeFile(
-			// 	'./data/' + index + '.json',
-			// 	JSON.stringify(currentGame),
-			// 	(err) => {
-			// 		if (err) {
-			// 			console.log('err = ' + err);
-			// 		}
-			// 	}
-			// );
-
-			data.push(currentGame);
 		}
+		writeFile(
+			'./data/' + (START_INDEX / 1000).toFixed(0) + '.json',
+			JSON.stringify(data),
+			(err) => {
+				if (err) {
+					console.log('err = ' + err);
+				}
+			}
+		);
 	}
-	writeFile(
-		'./data/' + (START_INDEX / 1000).toFixed(0) + '.json',
-		JSON.stringify(data),
-		(err) => {
-			if (err) {
-				console.log('err = ' + err);
-			}
-		}
-	);
 }
 
 main();
